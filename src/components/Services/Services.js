2c96fe5a4fd3e0c9cb6e0a02807c2a96f9623418
mollikacomputer <mollikacomputer3@gmail.com>
2022-11-14 00:09:53 +0600

    fetch data from jsonplaceholder link

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useServices from '../../Hooks/useServices';
+
 
 const Services = () => {
+    const [services] = useServices();
     return (
         <div>
             <h2 className='text-3xl'> Services</h2>
+            {services?.length}
+            {
+                services.map(service => <li> {service.name} </li> )
+            }
         </div>
     );
 };
