3d3a7c8af90bec0ee215a988cd21eeb18d517b5e
mollikacomputer <mollikacomputer3@gmail.com>
2022-11-13 22:37:36 +0600

    added tailwind css and daisy ui

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Registration = () => {
     return (
         <div>
-            <h2 className='txt-3xl'> Register now </h2>
+            <h2 className='text-3xl'> Register now </h2>
         </div>
     );
 };
