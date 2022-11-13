4a78d9746d1c1a8f812c3be2d330b7ed4cd2d6e4
mollikacomputer <mollikacomputer3@gmail.com>
2022-11-13 21:23:38 +0600

    first commit

---
+++ src/components/Home/Carousal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Carousal = () => {
+    return (
+        <div>
+            <h2> Slider</h2>
+        </div>
+    );
+};
+
+export default Carousal;
---
+++ src/components/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h2 className='txt-3xl'> Register now</h2>
+        </div>
+    );
+};
+
+export default Registration;
